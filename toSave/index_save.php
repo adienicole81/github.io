54fa22f4339ab7c67d9977624a5bb94ff505a839
adienicole <53946096+adienicole@users.noreply.github.com>
2019-12-17 14:18:35 -0500

    Commit test website
    Commit test website

--- toSave/index_save.php
+++ toSave/index_save.php
@@ -6,19 +6,17 @@
   <body>
     
     <h1>Hello, World!!!</h1>
-    <h2>Test Heading</h2>
     <h4 id='date'></h4>
-    <body>Test test blah blah blah</body>
 
     <img style="position:absolute;TOP:20px; LEFT:170px; width:500px;height:400px;" img src="images/Pic2.jpg">
     <img style="position:absolute;TOP:425px; LEFT:170px; width:400px;height:500px;" img src="images/pic.jpg">
     
-    <button class="btn"style="position:absolute;TOP:200px; RIGHT:170px; width:100px;height:50px;" onclick="window.location.href='page2.html'">Continue</button>
+    <button class="btn"style="position:absolute;TOP:200px; RIGHT:170px; width:100px;height:50px;" onclick="window.location.href='page2.php'">Continue</button>
     
   </body>
 
   
-  <link rel="stylesheet" type="text/css" href="./styles/styles.css" />  
+  <link rel="stylesheet" href="./styles/styles.css" />  
   <script async src="./javascript/index.js"></script>
   
 </html>
